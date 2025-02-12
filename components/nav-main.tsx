69f9eb9ae66f15beace283d6d1727fbc8404a57f
Supavasinan <vichaprs@gmail.com>
2025-02-12 21:22:41 +0700

    (refactor) use pathname hook to set active state for sidebar menu items

--- components/nav-main.tsx
+++ components/nav-main.tsx
@@ -9,6 +9,7 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
 } from "@/components/ui/sidebar";
+import { usePathname } from "next/navigation";
 
 export function NavMain({
   items,
@@ -19,13 +20,15 @@ export function NavMain({
     icon: LucideIcon;
   }[];
 }) {
+  const pathname = usePathname();
+
   return (
     <SidebarGroup className="group-data-[collapsible=icon]:hidden">
       <SidebarGroupLabel>Favorites</SidebarGroupLabel>
       <SidebarMenu>
         {items.map((item) => (
           <SidebarMenuItem key={item.title}>
-            <SidebarMenuButton asChild>
+            <SidebarMenuButton asChild isActive={pathname.startsWith(item.url)}>
               <a href={item.url} title={item.title}>
                 <item.icon />
                 <span>{item.title}</span>
