1e18a184f7fa4acd8a60a223157d5a12a742982d
Supavasinan <vichaprs@gmail.com>
2025-02-14 11:52:08 +0700

    (feat) refactor ThemeToggle to use checkbox for theme switching

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -3,37 +3,41 @@
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 
-import { Button } from "@/components/ui/button";
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
+import { useId } from "react";
 
 export function ThemeToggle() {
-  const { setTheme } = useTheme();
-
+  const { setTheme, theme } = useTheme();
+  const id = useId();
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme("light")}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("dark")}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme("system")}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="flex flex-col justify-center">
+      <input
+        type="checkbox"
+        name={id}
+        id={id}
+        className="peer sr-only"
+        checked={theme === "dark"}
+        onChange={() =>
+          setTheme((prev) => (prev === "dark" ? "light" : "dark"))
+        }
+      />
+      <label
+        className="group relative inline-flex size-9 cursor-pointer items-center justify-center rounded-lg border border-input bg-background text-foreground shadow-sm shadow-black/5 transition-colors hover:bg-accent hover:text-accent-foreground peer-focus-visible:outline peer-focus-visible:outline-2 peer-focus-visible:outline-ring/70"
+        htmlFor={id}
+        aria-label={`Switch to ${theme === "dark" ? "light" : "dark"} mode`}
+      >
+        <Moon
+          size={16}
+          strokeWidth={2}
+          className="shrink-0 scale-0 opacity-0 transition-all peer-checked:group-[]:scale-100 peer-checked:group-[]:opacity-100"
+          aria-hidden="true"
+        />
+        <Sun
+          size={16}
+          strokeWidth={2}
+          className="absolute shrink-0 scale-100 opacity-100 transition-all peer-checked:group-[]:scale-0 peer-checked:group-[]:opacity-0"
+          aria-hidden="true"
+        />
+      </label>
+    </div>
   );
 }
